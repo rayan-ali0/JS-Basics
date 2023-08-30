13da44dae7bcc38b4daa89567484b699e2ca509f
Rayan ALi <rayan.ali.8205@gmail.com>
2023-08-30 19:02:24 +0300

    basics-01-step-6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,5 @@
+var nb1=parseInt(prompt("Enter your first number :"));
+var nb2=parseInt(prompt("Enter your second number :"));
+var reminder=nb1%nb2;
+alert("The reminder of "+nb1+" and "+nb2+" is "+reminder);
+
