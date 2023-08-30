23d42faceff0fefaf14efaa1799342bd2c7cd41f
Rayan ALi <rayan.ali.8205@gmail.com>
2023-08-30 20:46:11 +0300

    basics-01-step-7

---
+++ javascript-basics-01/Step_7.js
@@ -0,0 +1,14 @@
+
+/*
+function calcul(){
+    var size=document.getElementById("shoe_size").value;
+var date=document.getElementById("year").value;
+    var result;
+    result=((size*2+5)*50)-date+1766;
+    alert(res)
+}
+*/
+var size=parseInt(prompt("Enter your shoe size :"));
+var date=parseInt(prompt("Enter your year of Birth :"));
+var result=((size*2+5)*50)-date+1766;
+alert(result);
