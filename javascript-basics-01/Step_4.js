2992d6c2fcebc970546786ef49b26536f9af4ae3
Rayan ALi <rayan.ali.8205@gmail.com>
2023-08-30 14:04:52 +0300

    basics-01-step-4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,5 @@
+var name=prompt("Enter your name:");
+var surname=prompt("Enter your surname:");
+var city=prompt("Enter your city:");
+var message="Name : "+name+"\nSurname : "+surname+"\nCity : "+city;
+alert(message)
