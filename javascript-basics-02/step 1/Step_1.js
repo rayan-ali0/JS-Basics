e6f9f4fa1a1fc2bb501c4b62895d4c2c73d2b9fb
Rayan ALi <rayan.ali.8205@gmail.com>
2023-08-31 14:32:02 +0300

    Basics-02-step-2

--- javascript-basics-02/step 1/Step_1.js
+++ javascript-basics-02/step 1/Step_1.js
@@ -1,6 +1,4 @@
 const img=document.querySelector("#image1");
-console.log(img)
 img.addEventListener("mouseover",function(){
-    // imgset="images/image1_2.jpg";
     img.setAttribute('src', './images/image1_2.jpg')
 });
---
+++ javascript-basics-02/step 2/Step_2.js
@@ -0,0 +1,4 @@
+const input=document.querySelector("#name");
+input.addEventListener("blur",function(){
+    alert('Thank your for your participating');
+})
